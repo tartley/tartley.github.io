81f6f7b34fbc336a0ac1f5a0270858a3677ea281
Jonathan Hartley <tartley@tartley.com>
2023-09-21 14:39:23 -0500

    Fix warning from Nikola about conf.py:BASE_URL

--- conf.py
+++ conf.py
@@ -20,10 +20,10 @@ BLOG_AUTHOR = "Jonathan Hartley"  # (translatable)
 BLOG_TITLE = "Made out of meat"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://www.tartley.com"
+SITE_URL = "https://www.tartley.com/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
-# BASE_URL = "https://tartley.com/"
+# BASE_URL = "https://www.tartley.com/"
 BLOG_EMAIL = "tartley @ tartley dot com"
 BLOG_DESCRIPTION = "A website devoted to oneself has been described — by my own Father, no less — as the greatest act of hubris. Welcome aboard!"  # (translatable)
 
